44117d241bbcbd8e52c3d3f9b0fc604344f1b2ce
Humberto Rivero <humbertorivero9@gmail.com>
2024-02-08 19:48:48 -0300

    fetching de datos del lado del cliente

---
+++ modulo-1/ecommerce/src/interface/character.ts
@@ -0,0 +1,14 @@
+
+export interface Character {
+	amiiboSeries: string;
+	character: string;
+	gameSeries: string;
+	image: string;
+	name: string;
+	tail: string;
+}
+
+export interface Serie {
+	key: string;
+	name: string;
+}
