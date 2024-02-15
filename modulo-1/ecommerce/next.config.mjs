5271d1a5b8611d530f8afef60b3f8230ecb30ccd
Humberto Rivero <humbertorivero9@gmail.com>
2024-02-15 19:48:04 -0300

    module3: soporte i18n nativo

--- modulo-1/ecommerce/next.config.mjs
+++ modulo-1/ecommerce/next.config.mjs
@@ -3,6 +3,10 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['raw.githubusercontent.com']
+  },
+  i18n: {
+    locales: ['es', 'en'],
+    defaultLocale: 'es'
   }
 };
 
